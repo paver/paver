c33539ccf88f6e05a4de27614a3b4a50657b4cb1
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-04-07 20:28:49 -0400

    added two new git commands: git.branch_list and git.branch_checkout

--- paver/git.py
+++ paver/git.py
@@ -6,11 +6,69 @@ At this point, these functions do not use any kind of library. They require
 the git binary on the path."""
 
 from paver.easy import sh, Bunch, path
+import os, re
+
+def _format_path(provided_path):
+    if provided_path:
+        return provided_path
+    else:
+        return os.getcwd()
 
 def clone(url, dest_folder):
     sh("git clone %(url)s %(path)s" % dict(url=url, path=dest_folder) )
 
 def pull(destination, remote="origin", branch="master"):
-    """Perform a git pull. Destination must be absolute path"""
+    """Perform a git pull. Destination must be absolute path.
+    
+    Will raise 
+    """
     sh("cd %(destination)s; git pull %(remote)s %(branch)s" % dict(
         destination=destination, remote=remote, branch=branch) )
+
+def branch_list(path="", __override__=None):
+    """Returns a Python tuple. The first item in the tuple will be the current
+    branch, and the other item will be a list of branches for the repository.
+    
+    Optional parameter path: the path to the git repo. Else uses os.getcwd()
+    """
+    
+    git_output = None
+    
+    if __override__ == None:
+        git_output = sh(  "cd %(repo_path)s; git branch" % dict(
+            repo_path = _format_path(path) )  )
+    else:
+        git_output = __override__
+    
+    if git_output == None:
+        return ( None, [] ) # should only hit this condition in testing...
+    
+    current_branch = ""
+    branches = []
+    found_a_match = False
+    regex = re.compile(r"(\*?)\W*(.+)")
+    
+    for line in git_output.split("\n"):
+        match_obj = regex.match(line)
+        if match_obj:
+            found_a_match = True
+            if match_obj.group(1):
+                current_branch = match_obj.group(2).strip()
+            if match_obj.group(2).strip():
+                branches.append( match_obj.group(2).strip() )
+        
+    if found_a_match == False:
+        raise "git branch did not return output expected. Returned %s" % (git_output)
+    
+    return (current_branch, branches)
+
+def branch_checkout(branch_name, path=""):
+    """Checkout a git branch.
+    
+    Take the branch name to checkout, and optional path parameter
+    (the path to the git repo. Else uses os.getcwd())
+    """
+    
+    sh( "cd %(repo_path)s; git checkout %(branch_name)s" % dict(
+        repo_path = _format_path(path),
+        branch_name=branch_name) )
--- paver/tests/test_git.py
+++ paver/tests/test_git.py
@@ -1,5 +1,7 @@
-from paver import git, runtime
+from paver import git, easy
 from paver.tests.mock import Mock, patch
+
+import os
 
 @patch(git, "sh")
 def test_simple_clone(sh):
@@ -12,3 +14,35 @@ def test_simple_pull(sh):
     git.pull("repo_path", "origin_remote", "master_branch")
     assert sh.called
     assert sh.call_args[0][0] == "cd repo_path; git pull origin_remote master_branch"
+
+@patch(git, "sh")
+def test_simple_branch_checkout(sh):
+    git.branch_checkout("my_branch", path="repo_path")
+    assert sh.called
+    assert sh.call_args[0][0] == "cd repo_path; git checkout my_branch"
+    
+@patch(git, "sh")
+def test_branch_chekout_cwd(sh):
+    """it should get the CWD and assume that is the repo"""
+    
+    git.branch_checkout("my_branch")
+    assert sh.called
+    assert sh.call_args[0][0] == "cd %(current_path)s; git checkout my_branch" % dict(
+        current_path=os.getcwd()
+    )
+    
+@patch(git, "sh")
+def test_branch_list(sh):
+    git.branch_list(path="repo_path")
+    assert sh.called
+    assert sh.call_args[0][0] == "cd repo_path; git branch"
+    
+@patch(git, "sh")
+def test_branch_list_correctly_parses_git_output(sh):
+    output = git.branch_list(path="repo_path", __override__="""
+* git_support
+  master
+  virtualenv_in_folder
+    """)
+    
+    assert output == ("git_support", ["git_support", "master", "virtualenv_in_folder"])
