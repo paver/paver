c58f0f75c8b86b6421f76994689f3bb5beccc2bd
Robin Kruppe <robin.kruppe@gmail.com>
2012-09-28 22:42:06 +0200

    Also adjust tox.ini, and fix an oversight

--- paver/doctools.py
+++ paver/doctools.py
@@ -121,7 +121,7 @@ class SectionedFile(object):
         self.contents = []
         self.sections = {}
         if from_string is not None:
-            from six import StringIO
+            from paver.deps.six import StringIO
             self._read_file(StringIO(from_string))
         else:
             with open(filename) as f:
