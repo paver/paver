33bb3de31813c47f97307bfcaabb7e778bcfdf78
Almad <bugs@almad.net>
2015-02-23 21:49:27 -0800

    Prepare for 1.2.4 release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ copyright = '2008, SitePen, Inc.'
 # The short X.Y version.
 version = '1.2'
 # The full version, including alpha/beta/rc tags.
-release = '1.2.3'
+release = '1.2.4'
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #today = ''
