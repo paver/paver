ae55fb557bc53ab8773af68baf135d921cb98db9
Almad <bugs@almad.net>
2017-11-25 18:22:39 +0100

    chore: Make integration test failures visible

--- tests_integration/test_virtualenv.py
+++ tests_integration/test_virtualenv.py
@@ -4,7 +4,7 @@ from unittest2 import TestCase
 from os import chdir, getcwd, pardir, environ
 from os.path import join, dirname, exists
 from shutil import rmtree, copyfile
-from subprocess import check_call, PIPE
+from subprocess import check_call, PIPE, STDOUT, CalledProcessError, check_output
 import sys
 from tempfile import mkdtemp
 
@@ -30,7 +30,11 @@ class TestVirtualenvTaskSpecification(TestCase):
         Use distribution's bootstrap to do so.
         """
         copyfile(join(dirname(__file__), pardir, "bootstrap.py"), join(self.basedir, "bootstrap.py"))
-        check_call([sys.executable, join(self.basedir, "bootstrap.py")], stdout=PIPE, stderr=PIPE, cwd=self.basedir)
+        try:
+            check_output([sys.executable, join(self.basedir, "bootstrap.py")], stderr=STDOUT, cwd=self.basedir)
+        except CalledProcessError as err:
+            print(err.output)
+            raise
 
     def test_running_task_in_specified_virtualenv(self):
         self._prepare_virtualenv()
