ed1858f2a9b718ab44752560beb913d87dff170d
Almad <bugs@almad.net>
2017-12-30 20:55:13 +0100

    Check for extra_files

--- tests_integration/test_minilib.py
+++ tests_integration/test_minilib.py
@@ -27,3 +27,33 @@ class TestMinilib(VirtualenvTestCase):
         assert exists(self.basedir / 'paver') == True
         assert exists(self.basedir / 'paver' / 'tasks.py') == True
 
+
+    def test_minilib_bundles_extra_files(self):
+        self.install_paver()
+        minilib_path = self.basedir / 'paver-minilib.zip'
+        
+        with open(self.basedir / 'pavement.py', 'w') as f:
+            f.write("""
+from paver.easy import *
+options = environment.options
+options(
+    minilib=Bunch(
+        extra_files=['ssh'],
+        versioned_name=False
+    )
+)      
+            """)
+
+        self.paver_execute('minilib')
+
+        assert exists(minilib_path) == True
+        # sanity check to prevent false positives
+        assert exists(self.basedir / 'paver') == False
+
+        with ZipFile(minilib_path, 'r') as zip:
+            zip.extractall(self.basedir)
+        
+        assert exists(self.basedir / 'paver') == True
+        assert exists(self.basedir / 'paver' / 'tasks.py') == True
+        assert exists(self.basedir / 'paver' / 'ssh.py') == True
+
