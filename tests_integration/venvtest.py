8065dd6491f71dcc964e3d91a3ef66bfe17ddbdc
Almad <bugs@almad.net>
2017-12-30 20:19:56 +0100

    Add simple minilib test

--- tests_integration/venvtest.py
+++ tests_integration/venvtest.py
@@ -3,7 +3,7 @@ from unittest2 import TestCase
 from os import chdir, getcwd, pardir, environ
 from os.path import join, dirname, exists
 from shutil import rmtree, copyfile
-from subprocess import check_call, PIPE, STDOUT, CalledProcessError #, check_output
+from subprocess import PIPE, STDOUT, CalledProcessError, check_output
 import sys
 from tempfile import mkdtemp
 
@@ -23,21 +23,25 @@ class VirtualenvTestCase(TestCase):
             from nose import SkipTest
             raise SkipTest("Integration tests not (yet) running on Windows/Appveor. Patches welcome.")
 
+        self.oldcwd = path(getcwd())
+        self.basedir = path(mkdtemp(prefix="test_paver_venv"))
+        self.paver_dev_dir = path(dirname(__file__)) / '..'
+
         self.prepare_virtualenv()
 
+        chdir(self.basedir)
+
+
     def prepare_virtualenv(self):
-        self.basedir = path(mkdtemp(prefix="test_paver_venv"))
-        self.oldcwd = path(getcwd())
 
         self.bootstrap_virtualenv()
 
-        self.paver_bin = join(dirname(__file__), pardir, 'distutils_scripts', 'paver')
-
-        # FIXME: Will this work on windows?
-        if 'VIRTUAL_ENV' in environ and exists(join(environ['VIRTUAL_ENV'], "bin", "python")):
-            self.python_bin = join(environ['VIRTUAL_ENV'], "bin", "python")
+        if sys.platform == 'win32':
+            self.site_packages_path = join(self.basedir, 'virtualenv', 'Lib', 'site-packages')
+            self.python_bin = self.basedir / 'virtualenv' / 'Scripts' / 'python.exe'
         else:
-            self.python_bin = "python"
+            self.site_packages_path = join(self.basedir, 'virtualenv', 'lib', 'python%s' % sys.version[:3], 'site-packages')
+            self.python_bin = self.basedir / 'virtualenv' / 'bin' / 'python'
 
 
     def bootstrap_virtualenv(self):
@@ -54,18 +58,24 @@ class VirtualenvTestCase(TestCase):
                               install_paver=False,
                               dest_dir=self.basedir / 'virtualenv')
 
-        # Use check_output instead of check_call once py26 and py32 support is dropped
         try:
-            # check_output([sys.executable, join(self.basedir, "bootstrap.py")], stderr=STDOUT, cwd=self.basedir)
-            check_call([sys.executable, self.basedir / "bootstrap.py"], stderr=STDOUT, cwd=self.basedir)
+            check_output([sys.executable, self.basedir / "bootstrap.py"], stderr=STDOUT, cwd=self.basedir)
         except CalledProcessError as err:
             # print(err.output)
             raise
 
-        if sys.platform == 'win32':
-            self.site_packages_path = join(self.basedir, 'virtualenv', 'Lib', 'site-packages')
-        else:
-            self.site_packages_path = join(self.basedir, 'virtualenv', 'lib', 'python%s' % sys.version[:3], 'site-packages')
+    def install_paver(self):
+        with self.paver_dev_dir:
+            check_output([self.python_bin, self.paver_dev_dir / 'setup.py', 'develop'])
+            if sys.platform == 'win32':
+                self.paver_bin = self.basedir / 'virtualenv' / 'Scripts' / 'paver'
+            else:
+                self.paver_bin = self.basedir / 'virtualenv' / 'bin' / 'paver'
+
+    def paver_execute(self, *args):
+        cmd = [self.paver_bin]
+        cmd.extend(args)
+        check_output(cmd)
 
     def tearDown(self):
         chdir(self.oldcwd)
