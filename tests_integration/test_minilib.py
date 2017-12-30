7f38148b796b4bb7924186cff5445177aac3e46a
Almad <bugs@almad.net>
2017-12-30 20:28:44 +0100

    Add better minilib checking

--- tests_integration/test_minilib.py
+++ tests_integration/test_minilib.py
@@ -5,13 +5,25 @@ from os.path import join, dirname, exists
 from shutil import rmtree, copyfile
 from subprocess import check_call, PIPE, STDOUT, CalledProcessError
 import sys
+from zipfile import ZipFile
 
 from .venvtest import VirtualenvTestCase
 
 class TestMinilib(VirtualenvTestCase):
 
-    def test_minilib_generated(self):
+    def test_minilib_contains_paver(self):
         self.install_paver()
         self.paver_execute('minilib')
 
-        assert exists('paver-minilib.zip') == True
+        minilib_path = self.basedir / 'paver-minilib.zip'
+
+        assert exists(minilib_path) == True
+        # sanity check to prevent false positives
+        assert exists(self.basedir / 'paver') == False
+
+        with ZipFile(minilib_path, 'r') as zip:
+            zip.extractall(self.basedir)
+        
+        assert exists(self.basedir / 'paver') == True
+        assert exists(self.basedir / 'paver' / 'tasks.py') == True
+
